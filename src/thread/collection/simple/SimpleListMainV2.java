b000723c2ed0a304918258c2d682b69ac65f4a1d
g1kwon <spring_shot@naver.com>
2025-02-03 22:38:05 -0500

    동시성 컬렉션이 필요한 이유 4 - proxy pattern

--- src/thread/collection/simple/SimpleListMainV2.java
+++ src/thread/collection/simple/SimpleListMainV2.java
@@ -2,16 +2,20 @@ package thread.collection.simple;
 
 import static util.MyLogger.log;
 
-import thread.collection.simple.list.SyncList;
+import thread.collection.simple.list.BasicList;
+import thread.collection.simple.list.SimpleList;
+import thread.collection.simple.list.SyncProxyList;
 
 public class SimpleListMainV2 {
 
   public static void main(String[] args) throws InterruptedException {
 //    test(new BasicList());
-    test(new SyncList());
+//    test(new SyncList());
+    BasicList basicList = new BasicList();
+    test(new SyncProxyList(basicList));
   }
 
-  private static void test(SyncList list) throws InterruptedException {
+  private static void test(SimpleList list) throws InterruptedException {
     log(list.getClass().getSimpleName());
 
     // A 를 리스트에 저장하는 코드
